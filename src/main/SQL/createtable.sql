8ad00d125294c2451427885adefacae10693ce0d
mkimball1 <lemortyjordan@gmail.com>
2025-01-16 09:58:10 -0800

    added sql files & started movielistservlet

---
+++ src/main/SQL/createtable.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE IF NOT EXISTS moviedb;
+USE moviedb;
+
+CREATE TABLE IF NOT EXISTS movies(
+                                     id VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    year INTEGER NOT NULL,
+    director VARCHAR(100) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS stars(
+                                    id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    birthYear INTEGER
+    );
+
+CREATE TABLE IF NOT EXISTS stars_in_movies(
+                                              starId VARCHAR(10) not null,
+    movieId VARCHAR(10) not null,
+    FOREIGN KEY (starId) REFERENCES stars(id),
+    FOREIGN KEY (movieId) REFERENCES movies(id)
+    );
+
+CREATE TABLE IF NOT EXISTS genres(
+                                     id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                     name VARCHAR(32) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS genres_in_movies(
+                                               genreId INTEGER NOT NULL,
+                                               movieId VARCHAR(10) NOT NULL,
+    FOREIGN KEY (genreId) REFERENCES genres(id),
+    FOREIGN KEY (movieId) REFERENCES movies(id)
+    );
+
+CREATE TABLE IF NOT EXISTS creditcards(
+                                          id VARCHAR(20) PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    expiration date NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS customers(
+                                        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                        firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    ccId VARCHAR(20) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    FOREIGN KEY (ccId) REFERENCES creditcards(id)
+    );
+
+CREATE TABLE IF NOT EXISTS sales(
+                                    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                    customerId INTEGER NOT NULL,
+                                    movieId VARCHAR(10) NOT NULL,
+    saleDate date NOT NULL,
+    foreign key (customerId) references customers(id),
+    foreign key (movieId) references movies(id)
+    );
+
+CREATE TABLE IF NOT EXISTS ratings(
+                                      movieId VARCHAR(10) NOT NULL,
+    rating FLOAT NOT NULL,
+    numVotes INTEGER NOT NULL,
+    foreign key (movieId) references movies(id)
+    );
--- src/main/java/MovieListServlet.java
+++ src/main/java/MovieListServlet.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
 
-public class MovieListServlet {
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serial;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@WebServlet(name = "StarsServlet", urlPatterns = "/api/movielist")
+public class MovieListServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace(); //fix
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        try (Connection conn = dataSource.getConnection()) {
+            Statement statement = conn.createStatement();
+            String query = "SELECT * from movies";
+            // Perform the query
+            ResultSet rs = statement.executeQuery(query);
+            JsonArray jsonArray = new JsonArray();
+
+            while (rs.next()) {
+                String star_id = rs.getString("id");
+                String star_name = rs.getString("name");
+                String star_dob = rs.getString("birthYear");
+
+                // Create a JsonObject based on the data we retrieve from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("star_id", star_id);
+                jsonObject.addProperty("star_name", star_name);
+                jsonObject.addProperty("star_dob", star_dob);
+
+                jsonArray.add(jsonObject);
+            }
+            rs.close();
+            statement.close();
+
+            request.getServletContext().log("getting " + jsonArray.size() + " results");
+
+            out.write(jsonArray.toString());
+            response.setStatus(200);
+
+        }catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            request.getServletContext().log("Error:", e);
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
 }
