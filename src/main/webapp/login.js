0acc04b63b89b2803f5ded47d54d4c3105ea27ed
mkimball1 <lemortyjordan@gmail.com>
2025-01-29 20:19:05 -0800

    login page done

---
+++ src/main/webapp/login.js
@@ -0,0 +1,57 @@
+function handleLoginResult(resultDataString) {
+    let resultDataJson = JSON.parse(resultDataString);
+
+    console.log("handle login response");
+    console.log(resultDataJson);
+    console.log(resultDataJson["status"]);
+
+    if (resultDataJson["status"] === "success") {
+        window.location.replace("index.html");
+    } else {
+        console.log("show error message");
+        console.log(resultDataJson["message"]);
+        $("#login_error_message").text(resultDataJson["message"]);
+    }
+}
+
+function submitLoginForm(formSubmitEvent) {
+    console.log("submit login form");
+    formSubmitEvent.preventDefault();
+
+    $.ajax({
+        url: "api/login",
+        method: "POST",
+        data: $("#login_form_element").serialize(),
+        success: handleLoginResult
+    });
+}
+
+function createLoginForm() {
+    let loginFormElement = $("#login_form");
+    let form = $("<form>", { id: "login_form_element" });
+
+    form.append(
+        $("<label>", { for: "email", text: "Email" }),
+        $("<br>"),
+        $("<input>", { type: "email", id: "email", name: "email", required: "true" }),
+        $("<br>"),
+        $("<label>", { for: "password", text: "Password" }),
+        $("<br>"),
+        $("<input>", { type: "password", id: "password", name: "password", required: "true" })
+    );
+
+    let button = $("<button>", {
+        text: "Submit",
+        type: "submit",
+        id: "submitButton"
+    });
+
+    form.append(button);
+    loginFormElement.append(form);
+
+    form.submit(submitLoginForm);
+}
+
+$(document).ready(function () {
+    createLoginForm();
+});
