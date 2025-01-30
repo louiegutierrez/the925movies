8b05e82a400f86460e695aa2a918007be58e1287
Leonardo Louis Parilla Gutierrez IV <65400514+ssaidwhotho@users.noreply.github.com>
2025-01-29 20:09:12 -0800

    Login Form Servlet Data

---
+++ src/main/java/FormServlet.java
@@ -0,0 +1,68 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "FormServlet", urlPatterns = "/api/login")
+public class FormServlet extends HttpServlet {
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // confirm from database if it contains this username and password
+        JsonObject responseJsonObject = new JsonObject();
+        try (Connection conn = dataSource.getConnection()){
+            String query = "SELECT * FROM customers WHERE email = ? AND password = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                request.getSession().setAttribute("username", new User(username));
+                responseJsonObject.addProperty("status", "success");
+                responseJsonObject.addProperty("message", "success");
+            } else {
+                responseJsonObject.addProperty("status", "error");
+                responseJsonObject.addProperty("message", "Incorrect password or username provided.");
+            }
+            response.getWriter().write(responseJsonObject.toString());
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            request.getServletContext().log("Error:", e);
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+
+
+    }
+}
