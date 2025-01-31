b902e042629e690d3bc70f468777b1ccea665b33
Leonardo Louis Parilla Gutierrez IV <65400514+ssaidwhotho@users.noreply.github.com>
2025-01-30 21:41:59 -0800

    OCD change

--- src/main/webapp/browse.js
+++ src/main/webapp/browse.js
@@ -1,4 +1,4 @@
-let letters = [0, 1, 2, 3, 4, 5, 6 ,7 ,8, 9, 'A', 'B', 'C', 'Z', '*']
+let letters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*'.split('')
 let genres = []
 
 function handleGenreResult(resultData){
