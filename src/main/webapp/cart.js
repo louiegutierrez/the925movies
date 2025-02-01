4db0209088cf1dac5f80f465411cd6c27104b2e3
mkimball1 <lemortyjordan@gmail.com>
2025-01-31 22:51:17 -0800

    added add/remove button functionality in cart

--- src/main/webapp/cart.js
+++ src/main/webapp/cart.js
@@ -7,18 +7,18 @@ function handleSessionData(resultDataJson) {
     $("#sessionID").text("Session ID: " + resultDataJson["sessionID"]);
     $("#lastAccessTime").text("Last access time: " + resultDataJson["lastAccessTime"]);
 
-
+    cartMovies.empty();
     Object.keys(resultDataJson.quantities).forEach(movieId => {
         let quantity = resultDataJson.quantities[movieId];
         let price = resultDataJson.prices[movieId];
 
         let rowHTML = `<tr>
-            <td>${movieId}</td>
+            <td>${resultDataJson["names"][movieId]}</td>
             <td>${quantity}</td>
-            <td>$${price.toFixed(2) * quantity}</td>
+            <td>$${(price* quantity).toFixed(2) }</td>
             <td>
-                <button class="addButton" data-movie-id="${movieId}">Add</button>
-                <button class="removeButton" data-movie-id="${movieId}">Remove</button>
+                <button class="addToCart refresh" data-movie-id="${movieId}">Add</button>
+                <button class="removeFromCart refresh" data-movie-id="${movieId}">Remove</button>
             </td>
         </tr>`;
         $("#cartTotal").text("Total: $" + resultDataJson["total"].toFixed(2));
@@ -57,13 +57,6 @@ function handleSessionData(resultDataJson) {
 // }
 //
 
-$.ajax("api/cart", {
-    method: "GET",
-    success: handleSessionData,
-    error: () => {
-        console.log("Failure");
-    }
-});
 
 
 $(document).on("click", ".addToCart", function () {
@@ -73,6 +66,7 @@ $(document).on("click", ".addToCart", function () {
         url: "api/cart",
         method: "POST",
         data: {
+            quantity: 1,
             movieId: movieId
         },
         success: function (response) {
@@ -85,3 +79,42 @@ $(document).on("click", ".addToCart", function () {
         }
     });
 });
+
+$(document).on("click", ".removeFromCart", function () {
+    let movieId = $(this).data("movie-id");
+    console.log("WOW! Movie ID:", movieId);
+    $.ajax({
+        url: "api/cart",
+        method: "POST",
+        data: {
+            quantity: -1,
+            movieId: movieId
+        },
+        success: function (response) {
+            console.log("Movie added to cart:", response);
+            alert("Movie added to cart");
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error("Error adding movie to cart:", textStatus, errorThrown);
+            console.error("Error adding movie to cart");
+        }
+    });
+});
+
+$(document).on("click", ".refresh", function() {
+    console.log("Refresh button clicked");
+    updateCart();
+});
+
+
+function updateCart(){
+    $.ajax("api/cart", {
+        method: "GET",
+        success: handleSessionData,
+        error: () => {
+            console.log("Failure");
+        }
+    });
+}
+
+updateCart();
