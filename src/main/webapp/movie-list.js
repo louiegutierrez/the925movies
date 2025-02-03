eca97edd08964f84380658395c1e5c1763c4e685
Leonardo Louis Parilla Gutierrez IV <65400514+ssaidwhotho@users.noreply.github.com>
2025-02-02 19:02:59 -0800

    Genres Linked

--- src/main/webapp/movie-list.js
+++ src/main/webapp/movie-list.js
@@ -44,7 +44,9 @@ function handleSearchResult(resultData) {
         rowHTML += `<td><a href="single-movie.html?id=${m.movie_id}">${m.title}</a></td>`;
         rowHTML += `<td>${m.year}</td>`;
         rowHTML += `<td>${m.director}</td>`;
-        rowHTML += `<td>${m.three_genres}</td>`;
+        let genreName = m.three_genres.split(", ");
+        let genresHTML = genreName.map(genre_name => `<a href="movie-list.html?genre=${genre_name}">${genre_name}</a>`).join(", ");
+        rowHTML += `<td>${genresHTML}</td>`;
         let starNames = m.three_stars.split(", ");
         let starIds = m.three_star_ids.split(", ");
         let starsHTML = starNames.map((name, j) => `<a href="single-star.html?id=${starIds[j]}">${name}</a>`).join(", ");
