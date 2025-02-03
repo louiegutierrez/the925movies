a3506a9e4901547120c5a68b0a620330e03e1927
Leonardo Louis Parilla Gutierrez IV <65400514+ssaidwhotho@users.noreply.github.com>
2025-02-02 20:19:38 -0800

    Displays Error Message in Checkout

--- src/main/webapp/checkout.js
+++ src/main/webapp/checkout.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-    $.ajax({
-        url: "api/cart",
-        method: "GET",
-        error: function () {
-            console.log("Failed to fetch total price.");
-        }
-    });
 
     $("#paymentForm").submit(function (event) {
         event.preventDefault();
@@ -21,7 +14,7 @@ $(document).ready(function () {
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.error("Payment request failed:", textStatus, errorThrown);
-                $("#message").text("An error occurred. Please try again.").css("color", "red");
+                $("#error-message").text("The credit card information is wrong. Please try again.");
             }
         });
     });
